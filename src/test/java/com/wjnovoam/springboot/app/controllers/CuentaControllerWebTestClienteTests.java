feb0c4cd613639b21dff2431311106b4a77dfdcf
William Novoa <wjnovoam@ufpso.edu.co>
2022-02-28 17:08:13 -0500

    Primer test real al controlador con WebTestClient

---
+++ src/test/java/com/wjnovoam/springboot/app/controllers/CuentaControllerWebTestClienteTests.java
@@ -0,0 +1,66 @@
+package com.wjnovoam.springboot.app.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wjnovoam.springboot.app.models.dto.TransaccionDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class CuentaControllerWebTestClienteTests {
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebTestClient client;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testTranferir() throws JsonProcessingException {
+
+        //Given
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setBancoId(1L);
+        dto.setMonto(new BigDecimal("100"));
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "Ok");
+        response.put("mensaje", "Transferencia realizada con exito!");
+        response.put("transaccion", dto);
+
+        //When
+        client.post().uri("http://localhost:8080/api/cuentas/transferir")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().jsonPath("$.mensaje").isNotEmpty()
+                .jsonPath("$.mensaje").value(is("Transferencia realizada con exito!"))
+                .jsonPath("$.mensaje").value((valor)-> assertEquals("Transferencia realizada con exito!", valor))
+                .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con exito!")
+                .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
+                .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
+                .json(objectMapper.writeValueAsString(response));
+    }
+}
